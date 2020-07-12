e96633e97ed385b5171aa680e5b2ea413f1c2920
kamalogudah <mcpaul2058@gmail.com>
2020-07-12 09:24:05 +0300

    Add Prod.exs

---
+++ config/prod.exs
@@ -0,0 +1,9 @@
+use Mix.Config
+
+config :at_ex,
+  api_key: "1eab43cb7d67fc7cccf2f393ad337b687b44adc96cd9292245363481583e2502",
+  content_type: "application/x-www-form-urlencoded",
+  accept: "application/json",
+  auth_token: "",
+  username: "sandbox",
+  endpoint: "sandbox"
